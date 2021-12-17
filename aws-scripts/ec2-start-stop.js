554de8230b42269f1bb68dcde0f3bf80cf2cbe44
Bradley <Bradley.plater1@gmail.com>
2021-12-17 15:44:02 +0000

    Added stop function

--- aws-scripts/ec2-start-stop.js
+++ aws-scripts/ec2-start-stop.js
@@ -1,12 +1,26 @@
 const AWS = require('aws-sdk');
 exports.handler = (event, context, callback) => {
     const ec2 = new AWS.EC2({ region: event.instanceRegion });
-    event.instanceId.forEach((id) => {
-        ec2.startInstances({ InstanceIds: [id] })
-            .promise()
-            .then(() =>
-                callback(null, `Successfully Started ${id} EC2 Instance`)
-            )
-            .catch((err) => callback(err));
-    });
+
+    if (event.Action == 'START') {
+        event.instanceId.forEach((id) => {
+            ec2.startInstances({ InstanceIds: [id] })
+                .promise()
+                .then(() =>
+                    callback(null, `Successfully Started ${id} EC2 Instance`)
+                )
+                .catch((err) => callback(err));
+        });
+    } else if (event.Action == 'STOP') {
+        event.instanceId.forEach((id) => {
+            ec2.stopInstances({ InstanceIds: [id] })
+                .promise()
+                .then(() =>
+                    callback(null, `Successfully Stopped ${id} EC2 Instance`)
+                )
+                .catch((err) => callback(err));
+        });
+    } else {
+        callback('Invalid Action provided', null);
+    }
 };
